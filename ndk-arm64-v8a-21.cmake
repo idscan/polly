4a46f6c7a2546e292778e4927030ff08c69234d8
John Forrest <john.forrest@gbgplc.com>
2019-11-25 12:35:57 +0000

    Switch to using NDK_ROOT instead of ANDROID_NDK Seems ANDROID_NDK is used within the toolchain, so effectively reserved

--- ndk-arm64-v8a-21.cmake
+++ ndk-arm64-v8a-21.cmake
@@ -1,10 +1,15 @@
 
+string(COMPARE EQUAL "$ENV{NDK_ROOT}" "" __empty_ndk_root_env)
+if(__empty_ndk_root_env)
+  message(FATAL_ERROR "No NDK_ROOT supplied")
+endif()
+
 # Set NEON - in theory redundant but to be clear
 set(ANDROID_ABI arm64-v8a)
 set(ANDROID_ARM_NEON TRUE)
 
 set(ANDROID_PLATFORM 21)
 
-include($ENV{ANDROID_NDK}/build/cmake/android.toolchain.cmake)
+include($ENV{NDK_ROOT}/build/cmake/android.toolchain.cmake)
 
 set(CMAKE_CXX_STANDARD 14) # Override the default
--- ndk-armeabi-v7a-19.cmake
+++ ndk-armeabi-v7a-19.cmake
@@ -1,11 +1,16 @@
 
+string(COMPARE EQUAL "$ENV{NDK_ROOT}" "" __empty_ndk_root_env)
+if(__empty_ndk_root_env)
+  message(FATAL_ERROR "No NDK_ROOT supplied")
+endif()
+
 # Assume neon
 set(ANDROID_ABI armeabi-v7a)
 set(ANDROID_ARM_NEON TRUE)
 
 set(ANDROID_PLATFORM 19)
 
-include($ENV{ANDROID_NDK}/build/cmake/android.toolchain.cmake)
+include($ENV{NDK_ROOT}/build/cmake/android.toolchain.cmake)
 
 set(CMAKE_CXX_STANDARD 14) # Override the default
 
