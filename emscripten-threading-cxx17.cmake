a4b77c6c02833f88b49566e3a3e52b798efab649
John Forrest <john.forrest@gbgplc.com>
2023-03-21 17:22:54 +0000

    ENT-8552 Add extra emscripten toolchain Add new one that adds -pthread so we can use threading

---
+++ emscripten-threading-cxx17.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2016, Alexandre Pretyman
+# All rights reserved.
+
+if(DEFINED POLLY_EMSCRIPTEN_CXX17_CMAKE)
+  return()
+else()
+  set(POLLY_EMSCRIPTEN_CXX17_CMAKE 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+    "Emscripten Cross Compile / Threading / C++17"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+include(polly_clear_environment_variables)
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/emscripten.cmake")
+
+polly_add_cache_flag(CMAKE_EXE_LINKER_FLAGS "-pthread")
+polly_add_cache_flag(CMAKE_SHARED_LINKER_FLAGS "-pthread")
+polly_add_cache_flag(CMAKE_CXX_FLAGS "-pthread")
+polly_add_cache_flag(CMAKE_C_FLAGS "-pthread")
