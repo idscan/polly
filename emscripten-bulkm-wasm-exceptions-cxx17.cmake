71951ffcc1243b3c73733cb3a66e37d508cf5a82
John Forrest <john.forrest@gbgplc.com>
2023-07-13 15:23:03 +0100

    ENT-9679 Add wasm toolchain with simd disabled (#29)
    Still use bulkm and proper exceptions but disable simd.
    
    Also fix the guards on the new(ish) wasm toolchains - were all the same.

---
+++ emscripten-bulkm-wasm-exceptions-cxx17.cmake
@@ -0,0 +1,25 @@
+# Copyright (c) 2016, Alexandre Pretyman
+# All rights reserved.
+
+if(DEFINED POLLY_EMSCRIPTEN_BULKM_WASM_EXCEPTIONS_CXX17_CMAKE)
+  return()
+else()
+  set(POLLY_EMSCRIPTEN_BULKM_WASM_EXCEPTIONS_CXX17_CMAKE 1)
+endif()
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_init.cmake")
+
+polly_init(
+	"Emscripten Cross Compile / Bulk Memory / WASM Exceptions / C++17"
+    "Unix Makefiles"
+)
+
+include("${CMAKE_CURRENT_LIST_DIR}/utilities/polly_common.cmake")
+include(polly_clear_environment_variables)
+include("${CMAKE_CURRENT_LIST_DIR}/flags/cxx17.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/compiler/emscripten.cmake")
+
+polly_add_cache_flag(CMAKE_EXE_LINKER_FLAGS "-mbulk-memory -mno-simd128 -fwasm-exceptions")
+polly_add_cache_flag(CMAKE_SHARED_LINKER_FLAGS "-mbulk-memory -mno-simd128 -fwasm-exceptions")
+polly_add_cache_flag(CMAKE_CXX_FLAGS "-mbulk-memory -mno-simd128 -fwasm-exceptions")
+polly_add_cache_flag(CMAKE_C_FLAGS "-mbulk-memory -mno-simd128 -fwasm-exceptions")
